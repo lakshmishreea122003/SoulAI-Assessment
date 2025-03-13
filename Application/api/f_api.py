fb773ef554f397f682f34502484d418b84ee9d19
Lakshmi Shree A <lakshmishreea122003@gmail.com>
2025-03-13 13:44:29 +0530

    Update f_api.py

--- Application/api/f_api.py
+++ Application/api/f_api.py
@@ -76,4 +76,4 @@ def predict_api():
     return jsonify({"predicted_class": predicted_class, "confidence": float(np.max(predictions))})
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8080, debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
